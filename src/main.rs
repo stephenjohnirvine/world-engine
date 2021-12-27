852e0a03f6e940e3f023cfe5c24d1be61673e9e0
Stephen Irvine <stephen.irvine@autodesk.com>
2021-12-27 14:19:49 +0000

    Format

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,21 @@
-use glam::{Vec2};
+use glam::Vec2;
 
 #[cfg(test)]
 mod world_state_tests;
 
 mod world_state;
 
-use world_state::{WorldState, City};
+use world_state::{City, WorldState};
 
 fn main() {
     println!("Hello, world!");
 
     let mut world_state = WorldState {
-        cities: vec![
-            City { 
-               position: Vec2::new(0.0, 0.0),
-               direction: Vec2::new(1.0, 0.0)
-            }
-        ]
+        cities: vec![City {
+            position: Vec2::new(0.0, 0.0),
+            direction: Vec2::new(1.0, 0.0),
+        }],
     };
 
     world_state = world_state.get_next_state();
-
 }
-
-
-
